bc687c57b8f98760b09790ca6b0cf10365707acb
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-02 05:46:04 -0700

    add print statements to the message received for 'afile_server'

--- src/afile_server.erl
+++ src/afile_server.erl
@@ -8,8 +8,10 @@ start(Dir) -> spawn(afile_server, loop, [Dir]).
 loop(Dir) ->
   receive
     {Client, list_dir} ->
+      io:format("list_dir cmd received~n"),
       Client ! {self(), file:list_dir(Dir)};
     {Client, {get_file, File}} ->
+      io:format("get_file cmd received~n"),
       Full = filename:join(Dir, File),
       Client ! {self(), file:read_file(Full)}
     end,

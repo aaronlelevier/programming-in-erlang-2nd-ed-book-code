1b7762b8bd28f627bdee86fc4ef7cd4bdfbb1d55
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-09 05:42:52 -0700

    shop.erl - re-did this file for 2nd pass of reading the book

--- src/shop.erl
+++ src/shop.erl
@@ -1,25 +1,11 @@
 -module(shop).
--export([cost/1, total/1, sum/1, total2/1, list_comp/1, tests/0]).
+-export([cost/1, total/1]).
 
-cost(oranges)	-> 5;
+cost(oranges)	  -> 5;
 cost(newspaper)	-> 8;
 cost(pears)     -> 1;
 cost(milk)      -> 4;
-cost(apples)	-> 2.
+cost(apples)  	-> 2.
 
 total([{What, N}|T]) -> cost(What)*N + total(T);
 total([]) -> 0.
-
-sum([H|T]) -> H + sum(T);
-sum([]) -> 0.
-
-total2(L) -> sum([shop:cost(What)*N || {What, N} <- L]).
-
-list_comp(L) -> [X || {_, X} <- L].
-
-tests() ->
-  Buy = [{milk,1}, {oranges,4}, {apples,2}],
-  10 = sum([2,3,5]),
-  [1,4,2] = list_comp(Buy),
-  28 = total2(Buy),
-  ok.

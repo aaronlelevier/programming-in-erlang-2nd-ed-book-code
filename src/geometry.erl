6f05784270021e95a1e0fb50d120a772be003668
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-08 18:16:55 -0700

    ch-4 example module 'geometry' first pass

---
+++ src/geometry.erl
@@ -0,0 +1,16 @@
+-module(geometry).
+
+-export([test/0, area/1]).
+
+test() ->
+  12 = area({rectangle, 3, 4}),
+  36 = area({square, 6}),
+  216.0 = area({triangle, 24, 30, 18}),
+  ok.
+
+
+area({rectangle, Width, Height}) -> Width * Height;
+area({square, Side}) -> Side * Side;
+area({triangle, Side1, Side2, Side3}) ->
+  P = (Side1 + Side2 + Side3) / 2,
+  math:sqrt(P*(P-Side1)*(P-Side2)*(P-Side3)).

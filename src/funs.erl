b3ff7121935ecec6dc874e8e4ede9b0123da526d
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-10 07:12:02 -0700

    up to pg 54, implemented my own filter even function

--- src/funs.erl
+++ src/funs.erl
@@ -1,5 +1,7 @@
 -module(funs).
--export([one_arg/0, mult_args/0, mult_clauses/0]).
+-export([
+  one_arg/0, mult_args/0, mult_clauses/0,
+  double_list/1, filter_even_orig/1, filter_even/1]).
 
 %% simple fun with a single arg
 one_arg() -> fun(X) -> X * 2 end.
@@ -14,3 +16,27 @@ mult_clauses() ->
   fun({c,C}) -> {f, 32 + C*9/5};
      ({f,F}) -> {c, (F-32)*5/9}
   end.
+
+
+%% ex. pg 54 - double all elements in a list
+double_list(L) ->
+  lists:map(fun(X) -> X * 2 end, L).
+
+
+%% ex pg. 54 filter a list using `lists:filter` std lib
+filter_even_orig(L) ->
+  lists:filter(fun(X) -> X rem 2 =:= 0 end, L).
+
+
+%% NOTE: the above actions are called "list-at-a-time" operations
+
+
+%% implement my own "filter even" w/o std lib
+filter_even([H|T]) ->
+  if
+    H rem 2 =:= 0 ->
+      [H|filter_even(T)];
+    true ->
+      filter_even(T)
+  end;
+filter_even([]) -> [].

45980a1f609516699d22f8b4f43ae31725092a35
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-11 06:38:56 -0700

    define your own control structures - example of how to recursively define a for loop
    functions that return functions
    
    lists:member - the 'in' list method

--- src/mylists.erl
+++ src/mylists.erl
@@ -1,20 +1,13 @@
 -module(mylists).
--export([filterEven/1, tests/0, sum/1, map/2, prepend_single/1, prepend_mult/2,
-    pop_head/1, pop_mult_head/1]).
+-export([filterEven/1, tests/0, prepend_single/1, prepend_mult/2,
+    pop_head/1, pop_mult_head/1, map/2, sum/1, make_test/1,
+    for/3]).
 
 tests() ->
     [2,4] = filterEven([1,2,3,4]),
     tests_worked.
 
 filterEven(L) -> lists:filter(fun(X) -> (X rem 2) =:= 0 end, L).
-
-
-sum([H|T]) -> H + sum(T);
-sum([]) -> 0.
-
-
-map(F, [H|T]) -> [F(H)|map(F,T)];
-map(_, []) -> [].
 
 
 prepend_single(X) ->
@@ -35,3 +28,21 @@ pop_head(List) ->
 pop_mult_head(List) ->
     [_,H1|_] = List,
     H1.
+
+
+map(_, []) -> [];
+map(F, [H|T]) -> [F(H)|map(F, T)].
+
+
+sum([H|T]) -> H + sum(T);
+sum([]) -> 0.
+
+
+%% fun that returns a fun
+make_test(L) ->
+    fun(X) -> lists:member(X, L) end.
+
+
+%% define your own for loop
+for(Max, Max, F) -> F(Max);
+for(I, Max, F) -> [F(I)|for(I+1, Max, F)].

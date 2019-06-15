c9f263a7d2ce305bbff93835943829970a72a79a
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-15 06:24:50 -0700

    WIP - echo client/server - not working

--- src/echo_client.erl
+++ src/echo_client.erl
@@ -1,10 +1,26 @@
 -module(echo_client).
 
--export([echo/2]).
+-export([echo/2, awake/1, start_listener/1, listener/1]).
 
 echo(Server, Msg) ->
   Server ! {self(), {echo, Msg}},
   receive
     {Server, ok} ->
       ok
+  end.
+
+start_listener(Server) -> spawn(echo_client, listener, [Server]).
+
+listener(Server) ->
+  receive
+    {Server, {whine, Msg}} ->
+      Msg
+  end,
+  listener(Server).
+
+awake(Server) ->
+  Server ! {self(), awake},
+  receive
+    {Server, ServerPid} ->
+      ServerPid
   end.
--- src/echo_server.erl
+++ src/echo_server.erl
@@ -1,6 +1,6 @@
 -module(echo_server).
 
--export([start/0, loop/0]).
+-export([start/0, loop/0, whine/1]).
 
 start() -> spawn(echo_server, loop, []).
 
@@ -8,6 +8,15 @@ loop() ->
   receive
     {Client, {echo, Msg}} ->
       io:format(Msg++"~n"),
-      Client ! {self(), ok}
+      Client ! {self(), ok};
+
+    {Client, awake} ->
+      Server2 = spawn(echo_server, whine, [Client]),
+      Client ! {self(), Server2}
   end,
   loop().
+
+whine(Client) ->
+  Client ! {self(), {whine, "Is anybody out there?"}},
+  timer:sleep(1000),
+  whine(Client).

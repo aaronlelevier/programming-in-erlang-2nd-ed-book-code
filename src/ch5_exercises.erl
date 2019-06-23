da6ea61c9f7821c8c4fe40eb881eebcf19052af0
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-23 07:24:23 -0700

    ch5_exercises.erl - reads a file and reads the first word and converts it into an atom

--- src/ch5_exercises.erl
+++ src/ch5_exercises.erl
@@ -10,7 +10,8 @@
 -author("aaron").
 
 %% API
--export([readlines/1]).
+-export([readlines/1, is_char/1, is_lower_char/1, is_upper_char/1,
+  config_to_tuple/1, read_config/2]).
 
 
 readlines(FileName) ->
@@ -24,3 +25,39 @@ get_all_lines(Device) ->
     eof  -> [];
     Line -> Line ++ get_all_lines(Device)
   end.
+
+
+config_to_tuple(FileName) ->
+  {ok, Device} = file:open(FileName, [read]),
+  Line = io:get_line(Device, ""),
+  read_config(Line, []).
+
+
+%% reads a string of "Chars" until runs into a non-char and converts
+%% the string to an atom
+read_config([], Chars) ->
+  list_to_atom(lists:reverse(Chars));
+read_config(Line, Chars) ->
+  [H|T] = Line,
+  IsChar = is_char(H),
+  if IsChar =:= false ->
+      read_config(T, Chars);
+    true ->
+      read_config(T, [H|Chars])
+  end.
+
+
+%% takes integer value of a char and returns book if a-z or A-Z
+is_char(CharInt) ->
+  is_lower_char(CharInt) or is_upper_char(CharInt).
+
+
+%% takes integer value of a char and returns a bool if the char is a-z
+%% i.e. "a" int value is 97, so true
+is_lower_char(CharInt) ->
+  (CharInt >= 97) and (CharInt =< 122).
+
+%% takes integer value of a char and returns a bool if the char is A-Z
+%% i.e. "a" int value is 97, so true
+is_upper_char(CharInt) ->
+  (CharInt >= 65) and (CharInt =< 91).

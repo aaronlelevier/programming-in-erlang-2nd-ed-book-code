89d2e3576ae31d3bf911bdf886ef668f5bd006de
aaronlelevier <aaron.lelevier@gmail.com>
2019-06-27 07:42:18 -0700

    create a module with methods from the Ruby hash class that might be used in Erlang - emtpy module just with docs on the problem statement

---
+++ src/ch5_ruby_hash.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%%% @author aaron
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%% Advanced: Look up the manual pages for the Ruby hash class.
+%%% Make a module of the methods in the Ruby class that you think
+%%% would be appropriate to Erlang.
+%%%
+%%% Ruby Hash class docs: https://ruby-doc.org/core-2.5.1/Hash.html
+%%%
+%%% @end
+%%% Created : 27. Jun 2019 07:36
+%%%-------------------------------------------------------------------
+-module(ch5_ruby_hash).
+-author("aaron").
+
+%% API
+-export([]).

96f0709b03ebea0954b269265a4b01f5913ba31a
aaronlelevier <aaron.lelevier@gmail.com>
2019-08-05 18:16:15 -0700

    commit originals of 'area_server' 1 and 2
    pre-commit to pg. 189 - 'Processes Are Cheap'

---
+++ src/concurrent/area_server0_original.erl
@@ -0,0 +1,25 @@
+%%%-------------------------------------------------------------------
+%%% @author aaron lelevier
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 02. Aug 2019 07:03
+%%%-------------------------------------------------------------------
+-module(area_server0_original).
+-author("aaron lelevier").
+
+-chapter(["12"]).
+
+%% API
+-export([loop/0]).
+
+loop() ->
+  receive
+    {rectangle, Height, Width} ->
+      io:format("Rectangle area is: ~p~n", [Height * Width]),
+      loop();
+    {square, Side} ->
+      io:format("Square area is: ~p~n", [Side * Side]),
+      loop()
+  end.
--- src/concurrent/area_server1_original.erl
+++ src/concurrent/area_server1_original.erl
@@ -6,35 +6,28 @@
 %%% @end
 %%% Created : 03. Aug 2019 23:57
 %%%-------------------------------------------------------------------
--module(area_server1).
+-module(area_server1_original).
 -author("aaron lelevier").
 
 %% API
--export([start/0, area/2, rpc/2, loop/0]).
-
-%% hides `spawn` from the user
-start() -> spawn(area_server1, loop, []).
-
-%% abstracts `rpc` and more accurately describes the function
-area(Pid, What) ->
-  rpc(Pid, What).
+-export([rpc/2, loop/0]).
 
 rpc(Pid, Request) ->
   Pid ! {self(), Request},
   receive
-    {Pid, Response} ->
+    Response ->
       Response
   end.
 
 loop() ->
   receive
     {From, {rectangle, Width, Ht}} ->
-      From ! {self(), Width * Ht},
+      From ! Width * Ht,
       loop();
     {From, {circle, R}} ->
-      From ! {self(), 3.14159 * R * R},
+      From ! 3.14159 * R * R,
       loop();
     {From, Other} ->
-      From ! {self(), {error, Other}},
+      From ! {error, Other},
       loop()
   end.

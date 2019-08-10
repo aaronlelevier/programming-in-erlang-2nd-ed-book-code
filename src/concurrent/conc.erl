65458626b14ad8c7318360a12a4825fecc298aa0
aaronlelevier <aaron.lelevier@gmail.com>
2019-08-10 08:49:44 -0700

    trying out diff types of call, receive and flush for msg passing
    call w/o a timeout - the process runs until a msg is received
    
    receive twice - receives two messages, only the 2nd msg is returned
    
    priority_receive_server - sends 2 messages - use to help understand msg sending and receiving and how messages are stored in the mailbox
    
    flush - retrieves and logs all messages in the mailbox

--- src/concurrent/conc.erl
+++ src/concurrent/conc.erl
@@ -2,13 +2,12 @@
 %%% @author aaron lelevier
 %%% @copyright (C) 2019, <COMPANY>
 %%% @doc
-%%%
+%%% Functions showing 2 methods communicating
 %%% @end
 %%% Created : 09. Aug 2019 07:10
 %%%-------------------------------------------------------------------
 -module(conc).
 -author("aaron lelevier").
-
 -chapter(["2"]).
 -vsn("v2").
 -desc("2nd read of chapter").
@@ -26,13 +25,42 @@ do_call_flow() ->
   call(Pid, "Hello").
 
 
+%% send a message and wait for a reply
 call(Pid, Request) ->
   Pid ! {self(), Request},
   receive
-    {Pid, Response} ->
+    Response ->
       Response
-  after 5000 ->
+  after 1000 ->
     io:fwrite("no response~n"),
+    false
+  end.
+
+
+%% same as book `rpc` method
+call_no_timeout(Pid, Request) ->
+  Pid ! {self(), Request},
+  receive
+    Response ->
+      Response
+  end.
+
+
+receive_twice() ->
+  receive
+    Response ->
+      io:fwrite("receive 1~n"),
+      % this value will be lost
+      Response
+  end,
+  receive
+    Response2 ->
+      io:fwrite("receive 2~n"),
+      % only this value is returned
+      Response2
+  after 0 ->
+    % only get in here if there's no match in the `receive`
+    io:fwrite("after 1~n"),
     false
   end.
 
@@ -40,7 +68,62 @@ call(Pid, Request) ->
 reply_looper() ->
   receive
     {From, Greeting} ->
-    From ! {self(), "Repling From:" ++ erlang:pid_to_list(self()) ++
-      " To:" ++ erlang:pid_to_list(From) ++ " Reply:" ++ Greeting},
-    reply_looper()
+      From ! {self(), "Repling From:" ++ erlang:pid_to_list(self()) ++
+        " To:" ++ erlang:pid_to_list(From) ++ " Reply:" ++ Greeting},
+      reply_looper()
+  end.
+
+
+%% priority_receive %%
+
+start_priority_receive() ->
+  Pid = spawn(?MODULE, priority_receive(), []),
+  cast(Pid, "Hello").
+
+
+priority_receive() ->
+  receive
+    {priority, Message} ->
+      io:fwrite("priority:~p~n", [Message]),
+      priority_receive();
+    Message ->
+      io:fwrite("normal:~p~n", [Message]),
+      priority_receive()
+  end.
+
+
+priority_receive_server() ->
+  receive
+    {From, {priority, Message}} ->
+      io:fwrite("priority:~p~n", [Message]),
+      From ! Message,
+      priority_receive_server();
+    {From, Message} ->
+      io:fwrite("normal:~p~n", [Message]),
+      From ! Message,
+      From ! "Reply two",
+      priority_receive_server();
+    Message ->
+      io:fwrite("no match:~p~n", [Message]),
+      Message,
+      priority_receive_server()
+  end.
+
+
+%% send a message and don't wait for a reply
+cast(Pid, Request) ->
+  io:fwrite("sending: ~p~n", [Request]),
+  Pid ! {self(), Request}.
+
+
+%% receive all messages in the Pid's mailbox
+flush() ->
+  receive
+    Response ->
+      io:fwrite("received: ~p~n", [Response]),
+      Response,
+      flush()
+  after 1000 ->
+    io:fwrite("no more messages~n"),
+    false
   end.

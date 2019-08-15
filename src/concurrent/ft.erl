5144442b683856fd19025981a6a98a447b0352c2
aaronlelevier <aaron.lelevier@gmail.com>
2019-08-15 07:12:40 -0700

    p. 209,  book code example, plus some code trying to hit the race condition noted in the book, but didn't hit the race condition

--- src/concurrent/ft.erl
+++ src/concurrent/ft.erl
@@ -58,3 +58,40 @@ list_to_atom_fun() ->
     end
   end.
 
+
+%% attempt to exploit race condition between `register` and `on_exit`
+%% call in `keep_alive` function
+%% didn't work, but the output from `ft:spawn_two_kills` is not
+%% idempotent. The # of processes started and killed each time
+%% varies from 2-4 after doing about 5 tests
+
+spawn_two_kills() ->
+  spawn(fun() -> start_keep_alive_kill() end),
+  spawn(fun() -> start_keep_alive_kill() end).
+
+
+start_keep_alive_kill() ->
+  Name = bob,
+  keep_alive(Name, list_to_atom_fun()),
+  for_kill(1, 5, Name),
+  % check if process was killed by "for" loop or `keep_alive` worked?
+  whereis(Name).
+
+
+keep_alive(Name, Fun) ->
+  register(Name, Pid = spawn(Fun)),
+  io:fwrite("respawn: ~p~n", [Pid]),
+  on_exit(Pid, fun(_Why) -> keep_alive(Name, Fun) end).
+
+
+kill(Name) ->
+  % send kill msg - Pid registered as "Name" converts a list()
+  % to an atom() so this will kill the process
+  Name ! 1.
+
+
+for_kill(Max, Max, Name) -> kill(Name);
+for_kill(Min, Max, Name) ->
+  io:fwrite("for_kill~n"),
+  kill(Name),
+  for_kill(Min+1, Max, Name).

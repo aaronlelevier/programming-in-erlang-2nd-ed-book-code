367b6f63e0ccf231f205e437ffdd8047d90324eb
aaronlelevier <aaron.lelevier@gmail.com>
2019-08-17 15:09:23 -0700

    move current 'storgier' dir to 'old_storagier'  because we're going to do a v2 :)

---
+++ src/concurrent/ch13_so.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author aaron lelevier
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17. Aug 2019 15:01
+%%%-------------------------------------------------------------------
+-module(ch13_so).
+-author("aaron lelevier").
+
+-compile(export_all).
+
+%% API
+-export([]).

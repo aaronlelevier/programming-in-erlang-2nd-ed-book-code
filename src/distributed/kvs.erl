7bc1971b3e0c8102e278eb328396d8e470f718fc
aaronlelevier <aaron.lelevier@gmail.com>
2019-08-17 20:42:51 -0700

    ch-14 Stage 1: A Simple Name Server - build a simple key/value lookup using message passing and separate processes

---
+++ src/distributed/kvs.erl
@@ -0,0 +1,57 @@
+%%%-------------------------------------------------------------------
+%%% @author aaron lelevier
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17. Aug 2019 20:24
+%%%-------------------------------------------------------------------
+-module(kvs).
+-author("aaron lelevier").
+-desc("A Simple Name Server - p. 214").
+
+-compile(export_all).
+
+%% API
+-export([]).
+
+
+-spec start() -> boolean().
+
+start() -> register(kvs, spawn(fun() -> loop() end)).
+
+
+-spec store(Key, Value) -> boolean() when
+  Key :: string(),
+  Value :: any().
+
+store(Key, Value) -> rpc({store, Key, Value}).
+
+
+-spec lookup(Key) -> {ok, Value} | undefined when
+  Key :: string(),
+  Value :: any().
+
+lookup(Key) -> rpc({lookup, Key}).
+
+
+%% Private API
+
+loop() ->
+  receive
+    {From, {store, Key, Value}} ->
+      put(Key, {ok, Value}),
+      From ! {kvs, true},
+      loop();
+    {From, {lookup, Key}} ->
+      From ! {kvs, get(Key)},
+      loop()
+  end.
+
+rpc(Q) ->
+  kvs ! {self(), Q},
+  receive
+    {kvs, Response} ->
+      Response
+  end.
+

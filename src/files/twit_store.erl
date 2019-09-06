2dd37ebc83a7f257cc4c053946cabac765b02759
aaronlelevier <aaron.lelevier@gmail.com>
2019-09-06 06:28:19 -0700

    ch-16 ex-6 - use random access file reading and writing to store and fetch tweets of 140 chars

--- src/files/twit_store.erl
+++ src/files/twit_store.erl
@@ -18,48 +18,58 @@
 -define(FILENAME, "data/twit-store.txt").
 
 %% the size of a twit
--define(TWIT_SIZE, 10).
+-define(TWIT_SIZE, 140).
 
 % placeholder `init` for Makefile
 init0() -> ok.
 
+start() ->
+  K = 10,
+  init(K),
+  % store twit at start of buf
+  store(0, "I went mtb'ing yesterday"),
+  fetch(0),
+  store(3, "I am Bob"),
+  fetch(3),
+  % store twit at end of buf
+  store(10, "You are Gary"),
+  fetch(10),
+  % fetch a non existent twit
+  fetch(5).
+
+
 %% functions from ex-6
 
 init(K) ->
+  io:format("init - K:~p~n", [K]),
   IoDevice = open_for_write(),
   file:pwrite(IoDevice, file_length(K), <<"\n">>),
   close(IoDevice).
 
 store(N, Str) ->
+  io:format("store - N:~p Str:~p~n", [N, Str]),
   Bin = pad(Str),
   store_buf(N, Bin).
 
 
-%% TODO: need to store string size in buffer, maybe at end, so the
-%% TODO: buffer can be destructured to retrieve the string
-%%130> twit_store:store(0, "Hello").
-%%ok
-%%131> twit_store:fetch(0).
-%%[72,101,108,108,111,0,0,0,0,10]
-%%132> Bin = <<72,101,108,108,111,0,0,0,0,10>>.
-%%<<72,101,108,108,111,0,0,0,0,10>>
-%%133> L = binary_to_list(Bin).
-%%[72,101,108,108,111,0,0,0,0,10]
-%%134> Str.
-%%* 1: variable 'Str' is unbound
-%%135> <<Str:5/binary, _/binary>> = Bin.
-%%<<72,101,108,108,111,0,0,0,0,10>>
-%%136> Str.
-%%<<"Hello">>
-%%137> binary_to_list(Str).
-%%"Hello"
-
 fetch(N) ->
+  io:format("fetch - N:~p~n", [N]),
   IoDevice = open_for_read(),
-  case file:pread(IoDevice, file_location(N), file_location(N+1)) of
-    {ok, Data} ->
+  case file:pread(IoDevice, file_location(N), file_location(N + 1)) of
+    {ok, Bin} ->
       close(IoDevice),
-      binary_to_list(Data);
+      Length = binary_tl_end(Bin),
+      <<Bin2:Length/binary, _/binary>> = Bin,
+      TwitStr = binary_to_list(Bin2),
+      case lists:sum(TwitStr) of
+        % throws an error if you try to access a non existent twit
+        0 ->
+          error(bad_index, [N]);
+        % success case
+        _ ->
+          io:format("fetch - TwitStr:~p~n", [TwitStr]),
+          TwitStr
+      end;
     Error ->
       close(IoDevice),
       Error
@@ -77,11 +87,11 @@ store_buf(N, Bin) ->
   close(IoDevice).
 
 open_for_write() ->
-  {ok, IoDevice} = file:open(?FILENAME, [write,raw,binary]),
+  {ok, IoDevice} = file:open(?FILENAME, [write, raw, binary]),
   IoDevice.
 
 open_for_read() ->
-  {ok, IoDevice} = file:open(?FILENAME, [read,raw,binary]),
+  {ok, IoDevice} = file:open(?FILENAME, [read, raw, binary]),
   IoDevice.
 
 close(IoDevice) ->
@@ -103,7 +113,15 @@ file_length(K) ->
 pad(Str) ->
   Bin = list_to_binary(Str),
   PadSize = (?TWIT_SIZE - size(Bin)) * 8,
-  <<Bin/binary, "\n":PadSize>>.
+  Length = length(Str),
+  <<Bin/binary, Length:PadSize>>.
+
+tl_end(L) ->
+  hd(lists:reverse(L)).
+
+binary_tl_end(Bin) ->
+  tl_end(binary_to_list(Bin)).
+
 
 %% example code - file:pread
 

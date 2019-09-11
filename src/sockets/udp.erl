6ba44d4647433e9d18f363bbf4fc7addb12be730
aaronlelevier <aaron.lelevier@gmail.com>
2019-09-11 06:01:19 -0700

    client/server using Ref example added - there's a lot of duplication though, may think about consolidating client/server pattern matching to handle if a Ref is included or not

--- src/sockets/udp.erl
+++ src/sockets/udp.erl
@@ -15,6 +15,8 @@
 
 -define(PORT, 4000).
 
+-define(TIMEOUT, 1000).
+
 % placeholder `init` for Makefile
 init() -> ok.
 
@@ -36,6 +38,24 @@ loop(Socket) ->
       loop(Socket)
   end.
 
+%% server that uses Ref
+
+server2(Port) ->
+  {ok, Socket} = gen_udp:open(Port, [binary]),
+  io:format("server opened socket:~p~n",[Socket]),
+  loop2(Socket).
+
+loop2(Socket) ->
+  receive
+    {udp, Socket, Host, Port, Bin} = Msg ->
+      io:format("server received:~p~n",[Msg]),
+      {Ref, N} = binary_to_term(Bin),
+      Fac = fac(N),
+      % because `gen_udp:open` got `binary` as an option, the reply here is binary
+      gen_udp:send(Socket, Host, Port, term_to_binary({Ref, Fac})),
+      loop(Socket)
+  end.
+
 %% client
 
 client(Request) ->
@@ -46,11 +66,35 @@ client(Request) ->
             {udp, Socket, _Host, _Port, Bin} = Msg ->
               io:format("client received:~p~n",[Msg]),
               {ok, binary_to_term(Bin)}
-          after 2000 ->
+          after ?TIMEOUT ->
             error
           end,
   gen_udp:close(Socket),
   Value.
+
+%% client that uses Ref
+
+client2(Request) ->
+  {ok, Socket} = gen_udp:open(0, [binary]),
+  io:format("client opened socket=~p~n",[Socket]),
+  Ref = make_ref(),
+  Request2 = term_to_binary({Ref, Request}),
+  ok = gen_udp:send(Socket, "localhost", ?PORT, Request2),
+  wait_for_ref(Socket, Ref).
+
+wait_for_ref(Socket, Ref) ->
+  receive
+    {udp, Socket, _Host, _Port, Bin} = Msg ->
+      io:format("client received:~p~n",[Msg]),
+      case binary_to_term(Bin) of
+        {Ref, Value} ->
+          Value;
+        {_SomeOtherRef, _Value} ->
+          wait_for_ref(Socket, Ref)
+      end
+  after ?TIMEOUT ->
+    error
+  end.
 
 %% functions
 
@@ -60,13 +104,27 @@ fac(N) when N > 0 ->
 
 %% starts client/server for testing
 
+%% client/server that doesn't use Ref
 start(N) ->
   ServerPid = spawn(fun() -> server(?PORT) end),
   % let server start
-  timer:sleep(1000),
+  timer:sleep(?TIMEOUT),
   io:format("requesting factorial of:~p~n", [N]),
 
   Ret = client(N),
+
+  io:format("received factorial of:~p~n", [Ret]),
+  stop(ServerPid),
+  ok.
+
+%% client/server that uses Ref
+start2(N) ->
+  ServerPid = spawn(fun() -> server2(?PORT) end),
+  % let server start
+  timer:sleep(?TIMEOUT),
+  io:format("requesting factorial of:~p~n", [N]),
+
+  Ret = client2(N),
 
   io:format("received factorial of:~p~n", [Ret]),
   stop(ServerPid),

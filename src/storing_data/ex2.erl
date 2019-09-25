c227175e1d7e7ac39d51ea68bca31cace7908f7c
aaronlelevier <aaron.lelevier@gmail.com>
2019-09-25 06:08:17 -0700

    ch-19 ex-2 - implement a counter with 'ets' for how many times certain lines of code are being called. Routines to init and read

--- src/storing_data/ex2.erl
+++ src/storing_data/ex2.erl
@@ -12,5 +12,51 @@
 -export([]).
 -chapter(["19"]).
 
+-define(COUNT(), init_or_inc_count({?MODULE, ?LINE})).
+
 % placeholder `init` for Makefile
-init() -> ok.
+start() ->
+  init(),
+  fac(5),
+
+  % read a key that exists
+  Key = ets:first(counts),
+  Val = read(Key),
+  io:format("Val:~p~n", [Val]),
+
+  % read a key that does not exist
+  Key2 = foo,
+  Val2 = read(Key2),
+  io:format("Val:~p~n", [Val2]),
+
+  L = ets:tab2list(counts),
+  io:format("~p~n~n", [L]).
+
+init() ->
+  ets:new(counts, [named_table]).
+
+read(Key) ->
+  ?COUNT(),
+  case ets:lookup(counts, Key) of
+    [] ->
+      ?COUNT(),
+      void;
+    [{Key, Val}] ->
+      ?COUNT(),
+      Val
+  end.
+
+fac(0) -> 1;
+fac(N) ->
+  ?COUNT(),
+  N * fac(N-1).
+
+init_or_inc_count(Key) ->
+  case ets:lookup(counts, Key) of
+    [] ->
+      ets:insert(counts, {Key, 1});
+    [{Key, Count}] ->
+      ets:insert(counts, {Key, Count+1});
+    Other ->
+      {error, Other}
+  end.

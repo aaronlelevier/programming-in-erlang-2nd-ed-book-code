bcc882e2321c954fd7a6610d9f4ca7347dd83b23
aaronlelevier <aaron.lelevier@gmail.com>
2019-09-29 07:32:07 -0700

    find first title of a page based on some search string
    uses , and only finds a single title
    
    Next things to do would be to find all titles, or do this more effciently with bitstring manipulation

--- src/commencal/web.erl
+++ src/commencal/web.erl
@@ -30,4 +30,42 @@ jersey_list_page_content() ->
       Bin
   end.
 
-%% TODO: next
+string_jersey_list_page_content() ->
+  Bin = jersey_list_page_content(),
+  binary_to_list(Bin).
+
+%% finds th first item's title
+%% returns:
+%%   "FOX FLEXAIR DELTA LONG SLEEVE JERSEY OPEN ORANGE 2019"
+extract_title() ->
+  StrH = "<h3 class=\"PBMainTxt\">",
+  StrT =  "</h3>",
+  S = string_jersey_list_page_content(),
+  [_|T] = string:split(S, StrH),
+  [H2|_] = string:split(T, StrT),
+  H2.
+
+acc_til_h3([Ha, Hb, Hc, Hd, He | T], Acc) ->
+  case {Ha, Hb, Hc, Hd, He} of
+    {$<, $/, $h, $3, $>} ->
+      Acc;
+    _ ->
+      acc_til_h3(T, [Hb, Hc, Hd, He | Acc])
+  end;
+acc_til_h3([], Acc) ->
+  lists:reverse(Acc).
+
+%% Jersey data
+%% title: FOX FLEXAIR DELTA LONG SLEEVE JERSEY OPEN ORANGE 2019
+
+string_to_binstring(S) ->
+  %%  S = "<h3 class=\"PBMainTxt\">"
+  L = [string:concat([X], "$ ") || X <- S],
+  S2 = string:trim(concat(L, [])),
+  list_to_tuple(string:lexemes(lists:reverse(S2), [$\s])).
+
+concat([], Acc) ->
+  Acc;
+concat([H|T], Acc) ->
+  Acc2 = string:concat(H, Acc),
+  concat(T, Acc2).

e6f5e3c77c6d279041d0d2f706f270da8c607fed
aaronlelevier <aaron.lelevier@gmail.com>
2019-10-12 06:24:46 -0700

    ch-22 ex-1 job_centre:add_job - func added

---
+++ src/otp_intro_genserver/job_centre.erl
@@ -0,0 +1,143 @@
+%%%-------------------------------------------------------------------
+%%% @author some name <me@hostname.local>
+%%% @copyright (C) 2013, some name
+%%% @doc
+%%%
+%%% @end
+%%% Created : 26 May 2013 by some name <me@hostname.local>
+%%%-------------------------------------------------------------------
+-module(job_centre).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+-compile(export_all).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+-include_lib("../otp_intro/macros.hrl").
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    {ok, _Pid} = gen_server:start_link({local, ?SERVER}, ?MODULE, [], []),
+    true.
+
+add_job(F) -> gen_server:call(?MODULE, {add_job, F}).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    {ok, queue:new()}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+%% adds a Job to the Queue. If the Queue is empty, start with
+%% JobNumber 1, else increment the Job number
+%% TODO: may need to change, for example if all jobs are done,
+%% TODO: we won't know the last JobNum from inspecting the Queue
+handle_call({add_job, F}, _From, Q) ->
+    Value = case queue:peek_r(Q) of
+        empty ->
+            NewJobNum = 1,
+            Q1 = queue:in({1, F}, Q),
+            NewJobNum;
+        {value, {JobNum, _F}} ->
+            NewJobNum = JobNum+1,
+            Q1 = queue:in({NewJobNum, F}, Q),
+            NewJobNum
+    end,
+    {reply, Value, Q1}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================

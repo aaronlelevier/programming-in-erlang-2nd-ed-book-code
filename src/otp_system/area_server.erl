659cd29213a33107c91b9e1bd8263237ae0b29c5
aaronlelevier <aaron.lelevier@gmail.com>
2019-11-01 06:40:48 -0700

    round_robin code works w/ area_server(gen_server) code to init gen_server's w/ diff names and send requests round robin to them

--- src/otp_system/area_server.erl
+++ src/otp_system/area_server.erl
@@ -11,21 +11,26 @@
 -include_lib("../macros.hrl").
 
 %% interface exports
--export([start_link/0, area/1, test/0]).
+-export([start_link/0, area/1, test/0, test2/0]).
 
 %% gen_server callback exports
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
   terminate/2, code_change/3]).
 
 %% interface
+%% each gen_server started will get a unique name, that is the
+%% module_name+index, which comes from the round_robin ETS table
 start_link() ->
-  Name = helpers:rand_atom(?MODULE),
+  Name = round_robin:add(?MODULE),
+  ?DEBUG(Name),
   gen_server:start_link({local, Name}, ?MODULE, [], []).
 
 %% public
+%% requests are sent round_robin
 area(Thing) ->
-  % TODO: call `round_robin:next(?MODULE)` here
-  gen_server:call(?MODULE, {area, Thing}).
+  Name = round_robin:next(?MODULE),
+  ?DEBUG(Name),
+  gen_server:call(Name, {area, Thing}).
 
 %% gen_server callbacks
 init(State) ->
@@ -39,8 +44,8 @@ handle_call({area, Thing}, _From, State) ->
 handle_cast(_Msg, State) -> {noreply, State}.
 handle_info(_Info, State) -> {noreply, State}.
 
-terminate(_Reason, _State) ->
-  ?DEBUG("terminatring"),
+terminate(Reason, State) ->
+  ?DEBUG({"terminatring", reason, Reason, state, State}),
   ok.
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
@@ -70,3 +75,15 @@ test() ->
   % will fail (on purpose)
   area({rectangle, 4, 5}).
 
+test2() ->
+  % start event manager
+  my_alarm_handler:start(),
+  % init round_robin ETS table
+  round_robin:init(),
+  % start 2 servers
+  start_link(),
+  start_link(),
+  % make requests
+  16 = area({square, 4}),
+  25 = area({square, 5}),
+  36 = area({square, 6}).
